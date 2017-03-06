5140e84c496d91e8cf9861bd483422c5615437d5
slopp <sean@rstudio.com>
2017-03-06 01:56:02 +0000

    a much better version that adds to the plot and inserts confusion matrices

--- inst/examples/census/toolchain/05_census_app.R
+++ inst/examples/census/toolchain/05_census_app.R
@@ -19,6 +19,7 @@ calcLift <- function(scored_data, cutoff = 0.5) {
 train <- read_csv("data/train.csv")
 test <- read_csv("data/test.csv")
 
+
 features <- tbl_vars(test) %>%
   .[-(. == "label")]
 
@@ -26,14 +27,18 @@ ui <- pageWithSidebar(
   HTML("<center><h1> Predicting Income From Census Data</h1></center> \n <center><h2> Is Your Income >50K? </h2></center>"),
   sidebarPanel(
     selectizeInput('selfeatures', 'Select Features:', features, multiple = TRUE),
+    sliderInput('cutoff', 'Cutoff Value', min = 0, max = 1, value = 0.5),
     actionButton('fit', "Add Model")
   ),
   mainPanel(
+    plotOutput("lift"),
     div(id = "models")
   )
 )
 
 server <- function(input, output, session) {
+
+  result <<- NULL
 
   observeEvent(input$fit, {
     withProgress(message = "Working...", value = 0.1, {
@@ -43,27 +48,46 @@ server <- function(input, output, session) {
 
       incProgress(0.5, detail = "Scoring Model")
       scored <- predict(model, test, type = "response")
-      test$pred <- scored
+      new <- test
+      new$pred <- scored
 
       incProgress(0.75, detail = "Evaluating Model")
       label <- paste0(input$fit, "model")
 
       insertUI(selector = "#models",
-        ui = plotOutput(label)
+        ui = HTML(paste(h3(f), tableOutput(label), collapse = "\n"))
       )
 
-      output[[label]]  <- renderPlot({
-       test %>%
-          calcLift %>%
-          ggplot(aes(x = bin, y = prop)) +
-          geom_point() + geom_line() +
-          labs(
-            title = paste("Lift Chart: ", f),
-            x = "",
-            y = ""
-          ) +
-          geom_abline(slope = 1/10, intercept = 0, color = "red")
+      output[[label]]  <- renderTable(rownames = TRUE, {
+        new$prediction <- ifelse(new$pred > input$cutoff, 1, 0)
+        conf_matrix <- matrix(data = rep(0,4), nrow = 2, ncol = 2)
+        conf_matrix[1,1] <- sum(new$prediction == 0 & new$label == 0 )
+        conf_matrix[1,2] <- sum(new$prediction == 1 & new$label == 0 )
+        conf_matrix[2,1] <- sum(new$prediction == 0 & new$label == 1 )
+        conf_matrix[2,2] <- sum(new$prediction == 1 & new$label == 1 )
+        colnames(conf_matrix) <- c("Predicted <=50K", "Predicted >50K")
+        rownames(conf_matrix) <- c("Actual <=50K", "Actual >50K")
+        conf_matrix
       })
+
+      output$lift <- renderPlot({
+
+        result <<- new %>%
+          calcLift(input$cutoff) %>%
+          mutate(model = f) %>%
+          rbind(result)
+
+        result %>%
+          ggplot(aes(x = bin, y = prop, color = model)) +
+            geom_point() + geom_line() +
+            labs(
+              title = "Lift Chart",
+              x = "",
+              y = ""
+            ) +
+            geom_abline(slope = 1/10, intercept = 0, color = "black")
+      })
+
     })
   })
 }
